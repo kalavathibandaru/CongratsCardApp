b55417df83263ab2d7e51db20e9a4cbafbd3c2be
kalavathibandaru <gn.kalavathi4@gmail.com>
2023-04-30 19:10:09 +0000

    CongratsCardApp

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading"> Congratulations </h1>
+    <div className="sub-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 className="sub-heading"> Kiran V </h1>
+      <p className="paragraph">
+        Vishnu Institute of Computer Education and Technology{" "}
+      </p>
+      <p className="paragraph"> Bhimavaram </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
